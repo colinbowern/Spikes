1f46d23fa6199e8c4515346c9916b8df511ae0dc
Colin Bowern <colin@bowern.com>
2013-11-20 12:16:48 -0500

    Adding MSDeploy indirect reference handling example

---
+++ msdeploy-indirect-references/Source/IndirectReference1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DirectReference1;
+
+namespace IndirectReference1
+{
+    public class Class1 : IDoSomething
+    {
+        public void Something()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
